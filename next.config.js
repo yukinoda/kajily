43d922a478b2ceb017725fd07629410810eb98c6
yukinoda <16083322+yukinoda@users.noreply.github.com>
2021-12-11 21:07:58 +0900

    feat: setup pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  // other next config
+})
